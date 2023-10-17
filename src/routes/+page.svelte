e478141eb5a3281e129f28662cad5b13a4982a75
erixweb <erikmorenodev@gmail.com>
2023-10-17 19:42:46 +0200

    Add header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,25 @@
 <svelte:head>
 	<title>Català Per Devs</title>
 </svelte:head>
-<nav class="w-full flex items-center gap-[20px] bg-[#DA121A] p-[20px] text-[white] uppercase">
-	<div>
-		<img
-			src="/catxdev.webp"
-			alt="Imatge de catala per desenvolupadors"
-			class="w-[70px] h-[70px]"
-		/>
-	</div>
-	<div class="inline-flex gap-[20px]">
-		<a href="/"> Inici </a>
-		<a href="#informacio"> Informació </a>
-	</div>
+<nav class="w-full bg-[#FCDD09] p-[20px] px-[30px] text-[#DA121A] font-semibold uppercase flex item-center justify-center">
+	<section class="w-full max-w-[1250px] flex flex-wrap items-center gap-[20px]">
+		<div>
+			<img
+				src="/catxdev.webp"
+				alt="Imatge de catala per desenvolupadors"
+				class="w-[70px] h-[70px]"
+			/>
+		</div>
+		<div class="inline-flex gap-[20px]">
+			<a href="/"> Inici </a>
+			<a href="#informacio"> Informació </a>
+		</div>
+	</section>
 </nav>
+<header class="w-full h-[450px] flex items-center justify-center text-center bg-[#DA121A] text-white">
+    <div>
+        <h1 class="text-[50px] font-bold tracking-[0.25em]">
+            CATALÀ PER DEVS
+        </h1>
+    </div>
+</header>   
