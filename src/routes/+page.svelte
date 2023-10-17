2b46035f1982fa5f54688b6cef83f65ae9483c5d
erixweb <erikmorenodev@gmail.com>
2023-10-17 19:21:33 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import "../styles.css"
+</script>
+
+<svelte:head>
+	<title>Català Per Devs</title>
+</svelte:head>
+<nav class="w-full flex items-center gap-[20px] bg-[#DA121A] p-[20px] text-[white] uppercase">
+	<div>
+		<img
+			src="/catxdev.webp"
+			alt="Imatge de catala per desenvolupadors"
+			class="w-[70px] h-[70px]"
+		/>
+	</div>
+	<div class="inline-flex gap-[20px]">
+		<a href="/"> Inici </a>
+		<a href="#informacio"> Informació </a>
+	</div>
+</nav>
